46054a5535e4ab8a43ef74c5361efe3f9935738f
trungnguyen338 <nguyentrungnguyen110621@gmail.com>
2025-11-04 08:05:44 +0700

    tao

--- Source.cpp
+++ Source.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b){
-	return a+b;
+int tong(int a, int b);
+int hieu(int a, int b){
+	return a-b;
 }
 
-
-
-int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
-
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
-	int tong = tong(5,6);
-	cout <<tong;
+	cout << " Hieu La" << hieu(10,5);
 	system("pause");
 	return 0;
 }
