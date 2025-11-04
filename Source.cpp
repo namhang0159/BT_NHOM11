0abd13308cd3f11aef0f168bea05bbc9953e4307
trungnguyen338 <nguyentrungnguyen110621@gmail.com>
2025-11-04 08:21:57 +0700

    them function Thuong

--- Source.cpp
+++ Source.cpp
@@ -9,12 +9,16 @@ int hieu(int a, int b){
 }
 
 int tich(int a, int b);
-float thuong(int a, int b);
+float thuong(int a, int b)
+{
+	return a/b;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong ="; <<tong(6,3);
 	cout << " Hieu La" << hieu(10,5);
+	cout << " Thuong la" << thuong(10,5);
 	system("pause");
 	return 0;
 }
