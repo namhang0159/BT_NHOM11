93acd74e7fb49429855db3bd996a9b6b64c4743e
Nam <namhang0159@gmail.com>
2025-11-04 07:53:21 +0700

    TH A

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b){
+	return a+b;
+}
+
+
+
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
+	int tong = tong(5,6);
+	cout <<tong;
 	system("pause");
 	return 0;
 }
