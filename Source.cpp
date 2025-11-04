f4bb0efcd4e15f989cd733a5c922c27443fd94dc
Nam <namhang0159@gmail.com>
2025-11-04 07:59:47 +0700

    testNam

--- Source.cpp
+++ Source.cpp
@@ -10,13 +10,17 @@ int tong(int a, int b){
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
-
+void Name(){
+	cout<<"Nam";
+	
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
 	int tong = tong(5,6);
 	cout <<tong;
+	Name();
 	system("pause");
 	return 0;
 }
